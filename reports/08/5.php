ae4e686e33b684af5fdeb4e759d66ab830de9f39
GwonDH <144209296+GwonDH@users.noreply.github.com>
2023-11-19 23:51:39 +0900

    Create 5.php

---
+++ reports/08/5.php
@@ -0,0 +1,10 @@
+<?php
+$filename = "client.txt";
+
+$file = fopen($filename, 'r');
+
+while ($line = fgets($file)) {
+    $content = explode(' ', $line);
+    if (intval($content[1]) >= 30) echo "Name : ",$content[0],", Age : ",$content[1],", Gender : ",$content[2],", Email : ",$content[3],"<br>";
+}
+?>
